7a64f6eb3f35117bf5d26bbb2cd918a171261073
Norbert Demeny <norbi.demeny.3pg@cofense.com>
2020-12-03 16:47:51 +0200

    Project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <transition name="app_a-fade" mode="out-in">
+      <router-view :key="$route.path" />
+    </transition>
+  </div>
+</template>
