2c18db3650ede3c2f06552bc970b25c629a72cb8
unknown <gdsroyal@gmail.com>
2021-10-13 14:50:19 +0300

    Replaced en with ro

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -17,8 +17,8 @@ function loadLocaleMessages(): LocaleMessages {
   return messages;
 }
 const i18n = new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: process.env.VUE_APP_I18N_LOCALE || 'ro',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'ro',
   messages: loadLocaleMessages(),
 });
 
