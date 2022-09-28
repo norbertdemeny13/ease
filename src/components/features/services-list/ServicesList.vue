aaa8903c50deaba86220ebdae4c075cf065f7e3d
Florin Peslar <florin.peslar@gmail.com>
2022-09-28 20:18:45 +0300

    removed unused code

--- src/components/features/services-list/ServicesList.vue
+++ src/components/features/services-list/ServicesList.vue
@@ -31,15 +31,6 @@
             :to="getToRoute(item)"
           />
         </div>
-        <!-- <template v-else>
-          <div v-for="item in service.items" :key="item.category" class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-            <services-list-item
-              :image-path="getImagePath(item)"
-              :service="item"
-              :to="getToRoute(item)"
-            />
-         </div>
-        </template> -->
       <!-- /strip grid -->
     </div>
   </div>
